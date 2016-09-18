8418f200cba99ce440c6203e6496edb3c2b93aaa
Ethan Krish <ethankrish@live.com>
2016-09-18 16:13:47 -0500

    connected camera

---
+++ moalpic/motionalarm.py
@@ -0,0 +1,54 @@
+import sys
+import traceback
+import RPi.GPIO as GPIO
+from pygame import mixer
+import time
+import picamera
+
+motionpin=18
+
+def take_picture():
+    camera = picamera.PiCamera()
+    camera.capture('image.jpg')
+
+def initialize():
+    mixer.init()
+    mixer.music.load("/home/pi/research/motionalarm/intruder2.mp3")
+	
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(motionpin, GPIO.IN)
+
+def motion_stopped():
+    print "clear"
+    mixer.music.stop()
+
+def motion_detected():
+    print "intruder!"
+    mixer.music.play()
+    take_picture()
+
+def uninitialize():
+    GPIO.cleanup()
+    mixer.quit()
+
+
+def main_loop():
+    oldvalue=-1
+    while True:
+        i=GPIO.input(motionpin)
+        if i != oldvalue:
+            if i==0:
+                motion_stopped()
+            else:
+                motion_detected()
+        time.sleep(0.1)
+        oldvalue = i
+
+
+try:
+    initialize()
+    main_loop()
+except: 
+    uninitialize()
+    traceback.print_exc(file=sys.stdout)
